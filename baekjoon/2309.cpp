b40d8c82cc45d41feb3ed47bcdb1884c04135a56
JaeSukHwnag <dajasin245@naver.com>
2019-01-22 17:28:52 +0900

    BF

---
+++ baekjoon/2309.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a[9];
+	int sum = 0;
+	for(int i=0; i<9; i++)
+	{
+	scanf("%d", &a[i]);
+	sum = sum + a[i];
+	}
+	
+	sort(a, a + 9);
+	
+	for(int i=0; i<8; i++)
+		{
+		for(int j=i+1; j<9 ;j++)
+			{
+				if(sum- a[i] - a[j]==100)
+				{
+				for(int k=0; k<9; k++)
+				{
+					if(k == i || k == j)continue;
+					printf("%d\n", a[k]);
+				}
+				return 0;
+				}
+		}
+			}
+	return 0;
+}
